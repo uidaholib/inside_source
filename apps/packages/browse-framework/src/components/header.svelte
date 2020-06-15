96a10c3796efa58e8419fcfa5471911dd89300d2
evanwill <evanpeterw@gmail.com>
2020-06-15 11:17:25 -0700

    delete apps

--- apps/packages/browse-framework/src/components/header.svelte
+++
@@ -1,3 +0,0 @@
-<header class="flex bg-brand-green text-white px-6 py-2 shadow-lg">
-    <slot></slot>
-</header>
--- apps/packages/search/src/main.js
+++
@@ -1,9 +0,0 @@
-import App from './app.svelte';
-
-const target = document.getElementById("inside-idaho-search-app");
-
-const app = new App({
-  target: target
-});
-
-export default app;
